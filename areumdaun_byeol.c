8cb8d425d2a3aec7a41aad1a34a96834d28ab81b
ultisy <youngui64@gmail.com>
2024-08-21 12:15:37 +0900

    아름다운 별

---
+++ areumdaun_byeol.c
@@ -0,0 +1,11 @@
+// areumdaun_byeol.c - ByeolEul JJiekseumnida.
+#include <stdio.h>
+void main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < i; j++)
+			printf("*");
+		printf("\n");
+	}
+}
